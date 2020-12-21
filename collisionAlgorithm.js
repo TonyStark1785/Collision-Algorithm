c12976db2f6acae0f33adf269c12a9a86adecf71
TonyStark1785 <75624111+TonyStark1785@users.noreply.github.com>
2020-12-21 19:42:54 +0530

    Add files via upload

---
+++ collisionAlgorithm.js
@@ -0,0 +1,55 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  gameObject1=createSprite(100, 100, 50, 50);
+  gameObject1.shapeColor="blue";
+  gameObject2=createSprite(200, 100, 50, 50);
+  gameObject2.shapeColor="white";
+  gameObject3=createSprite(300, 100, 50, 50);
+  gameObject3.shapeColor= "lime";
+  gameObject4=createSprite(400, 100, 50, 50);
+  gameObject4.shapeColor="cyan";
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+   
+  if (isTouching(movingRect, gameObject1)){
+    movingRect.shapeColor = "red";
+    gameObject1.shapeColor = "red";
+  }
+  else if (isTouching(movingRect, gameObject2)){
+    movingRect.shapeColor = "red";
+    gameObject2.shapeColor = "red";
+  }
+  else if (isTouching(movingRect, gameObject3)){
+    movingRect.shapeColor = "red";
+    gameObject3.shapeColor = "red";
+  }
+  else if (isTouching(movingRect, gameObject4)){
+    movingRect.shapeColor = "red";
+    gameObject4.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject1.shapeColor = "blue";
+    gameObject2.shapeColor = "white";
+    gameObject3.shapeColor = "lime";
+    gameObject4.shapeColor = "cyan";
+  }
+
+
+  drawSprites();
+}
+
